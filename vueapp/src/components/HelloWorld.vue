6a06109d69603972b8220e7adb9503b508ca5add
SteelBurrito <antouniokevin@gmail.com>
2017-12-12 11:20:50 +0700

    Added new vue component and editing helloworld

--- vueapp/src/components/HelloWorld.vue
+++ vueapp/src/components/HelloWorld.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <hr />
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.users">
+          {{user.firstname}} {{user.lastname}}
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <div>
+      <input type="text" v-model="input_val">
+    </div>
+    <div>
+      Input Value: <span v-text="input_val"></span>
+    </div>
+    <hr />
+    <div>
+      <button class="btn btn-primary" v-on:click="counter++">You've clicked this button {{counter}} times!</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome',
+      users: [
+        {firstname: 'Jimmy', lastname: 'James'},
+        {firstname: 'Porter', lastname: 'Smith'},
+        {firstname: 'Bob', lastname: 'Fronkle'}
+      ],
+      input_val: '',
+      counter: 0
     }
   }
 }
@@ -37,12 +47,7 @@ h1, h2 {
   font-weight: normal;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+    list-style-position: inside;
 }
 a {
   color: #42b983;
